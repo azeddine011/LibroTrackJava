8136ba814e97fa50552a70f19891b7c8428fca5e
taki <taki.azeddine@yobeen.com>
2025-05-24 12:11:22 +0100

    init commit

---
+++ src/main/java/com/librotrackapp/librotrack/LibrotrackApplication.java
@@ -0,0 +1,13 @@
+package com.librotrackapp.librotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrotrackApplication.class, args);
+	}
+
+}
